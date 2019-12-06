563a4c3f34c4d1712e003f54fd1166b9c0a0b391
Jens Bäckvall <jens@entropyfox.com>
2019-12-06 11:33:12 +0100

    redo sql in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const sqlStatements = require('./backend/sqlMethods');
+const sqlInstance = require('mssql');
 
 const port = 3000;
 
@@ -10,9 +10,30 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// configuration for database
+const configDB = {
+    user: 'sa',
+    password: 'DataBaseExam2019',
+    server: 'localhost',
+    database: 'DataBaseExam2019'
+};
+
 app.get('/', function (req, res) {
-    fetchAllProducts;
-});
+    sqlInstance.connect(configDB, function (err) {
+        if (err) console.log(err);
+        // create Request object
+        var request = new sqlInstance.Request();
+        // query to the database and get the products
+        request.query('select * from TProduct', function (err, products) {
+            if (err) console.log(err)
+            // send records as a response
+            console.log(products);
+            res.status(200).json({
+                products: products
+            });
+        });
+        
+})});
 
 
 
