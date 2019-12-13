8a3d0af929fde609a2c237f52fccaf096b8f78db
Luis Osorio Lutzen <luis.lutzen@gmail.com>
2019-12-13 15:48:06 +0100

    Rating needs the Jens Touch

--- public/productview/productview.js
+++ public/productview/productview.js
@@ -14,4 +14,31 @@ $.ajax({
     $(".product").append(htmlString);
 
 
+/*
+
+For testing
+
+    const ratings = {
+        hotel_a : 2.8,
+        hotel_b : 3.3,
+        hotel_c : 1.9,
+        hotel_d : 4.3,
+        hotel_e : 4.74
+      };
+
+    const starTotal = 5;
+ 
+    
+  for(const rating in ratings) {  
+    
+    const starPercentage = (ratings[rating] / starTotal) * 100;
+    
+    const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
+    
+    document.querySelector(`.${rating}.stars-inner`).style.width = starPercentageRounded; // ${rating}
+  }
+ 
+*/
+
+  document.querySelector(`.${thisProduct.nAverageRating}.stars-inner`).style.width = starPercentageRounded; // ${rating} 
 });
